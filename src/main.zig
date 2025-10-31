29be31d6c9ed64f2535bcb262334cc7c5c7dbb1a
ورد <kasimeka@tuta.io>
2025-10-31 21:26:25 +0200

    simplify encoder

--- src/main.zig
+++ src/main.zig
@@ -1,34 +1,21 @@
 const std = @import("std");
-const base64 = @import("base64"); // my implementation
+const base64 = @import("base64");
 
 pub fn main() !void {
     const buf_len = 100;
 
-    var out_buf: [buf_len]u8 = undefined;
-    var stdout = std.fs.File.stdout().writer(&out_buf);
-
     var in_buf: [buf_len]u8 = undefined;
     var stdin = std.fs.File.stdin().reader(&in_buf);
 
-    // i lowk hate this
-    var n: u8 = 0;
-    var read_buf: [buf_len]u8 = undefined;
-    while (stdin.interface.takeByte()) |c| {
-        read_buf[n] = c;
-        n += 1;
-    } else |e| {
-        if (e != error.EndOfStream) return e;
-    }
-    const str = read_buf[0..n];
-    // is it really the best i can do?
-
     var allocator = std.heap.DebugAllocator(.{}).init;
-    defer _ = allocator.deinit();
     const gpa = allocator.allocator();
 
-    const b64 = try base64.Base64.init.encode(gpa, str);
+    const str = try stdin.interface.allocRemaining(gpa, .unlimited);
+    const b64 = try base64.encode(gpa, str);
     defer gpa.free(b64);
 
+    var out_buf: [1.4 * buf_len]u8 = undefined;
+    var stdout = std.fs.File.stdout().writer(&out_buf);
     try stdout.interface.print("{s}\n", .{b64});
     try stdout.interface.flush();
 }
--- src/root.zig
+++ src/root.zig
@@ -1,47 +1,61 @@
 const std = @import("std");
 
-pub const Base64 = struct {
-    table: *const [64]u8 =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ++
-        "abcdefghijklmnopqrstuvwxyz" ++
-        "0123456789+/",
+pub const char_table: *const [64]u8 =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ++
+    "abcdefghijklmnopqrstuvwxyz" ++
+    "0123456789+/";
 
-    const Self = @This();
-    pub const init = Self{};
+const PLAIN_GROUP_SIZE = 3;
+const ENCODED_GROUP_SIZE = 4;
 
-    pub fn encode(self: Base64, gpa: std.mem.Allocator, str: []const u8) ![]u8 {
-        if (str.len == 0) return "";
+pub fn encode(gpa: std.mem.Allocator, input: []const u8) ![]u8 {
+    if (input.len == 0) return "";
 
-        const ngroups = try std.math.divCeil(usize, str.len, 3);
-        const outsize = 4 * ngroups;
+    const group_count = try std.math.divCeil(usize, input.len, PLAIN_GROUP_SIZE);
+    var output = try gpa.alloc(u8, ENCODED_GROUP_SIZE * group_count);
 
-        var out = try gpa.alloc(u8, outsize);
+    for (0..group_count) |g| {
+        const group_offset_in_input = PLAIN_GROUP_SIZE * g;
 
-        for (0..ngroups) |g| {
-            const str_offset = 3 * g;
-            const count = @min(str.len - str_offset, 3);
-            const group = str[str_offset .. str_offset + count];
+        const group_len = @min(PLAIN_GROUP_SIZE, input.len - group_offset_in_input);
+        var group_buf: [PLAIN_GROUP_SIZE]u8 = @splat(0);
+        @memcpy(group_buf[0..group_len], input[group_offset_in_input..][0..group_len]);
 
-            var buf: [4]u8 = undefined;
-            buf[0] = group[0] >> 2;
-            buf[1] = (0b0011 & group[0]) << 4 |
-                if (group.len > 1) group[1] >> 4 else 0;
-            buf[2] = (if (group.len > 1) (0b1111 & group[1]) << 2 else 0) |
-                if (group.len > 2) group[2] >> 6 else 0;
-            buf[3] = if (group.len > 2) (0b00111111 & group[2]) else 0;
-
-            const out_offset = 4 * g;
-            inline for (0..4) |n| {
-                out[out_offset + n] = self.table[buf[n]];
-            }
-        }
-
-        for (@subWithOverflow(out.len, 3).@"0"..out.len) |n| {
-            if (out[n] == 'A') {
-                out[n] = '=';
-            }
-        }
-
-        return out;
+        const group_offset_in_output = ENCODED_GROUP_SIZE * g;
+        const out_group = output[group_offset_in_output..][0..ENCODED_GROUP_SIZE];
+        out_group[0] = char_table[group_buf[0] >> 2];
+        out_group[1] = char_table[(0b0011 & group_buf[0]) << 4 | group_buf[1] >> 4];
+        out_group[2] = char_table[(0b1111 & group_buf[1]) << 2 | group_buf[2] >> 6];
+        out_group[3] = char_table[0b00111111 & group_buf[2]];
     }
-};
+
+    const pad_len =
+        (PLAIN_GROUP_SIZE - input.len % PLAIN_GROUP_SIZE) % PLAIN_GROUP_SIZE;
+    for (0..pad_len) |n| {
+        output[output.len - 1 - n] = '=';
+    }
+
+    return output;
+}
+
+pub fn decode(gpa: std.mem.Allocator, str: []const u8) ![]u8 {
+    if (str.len == 0) return "";
+    const ngroups = try std.math.divCeil(usize, str.len, ENCODED_GROUP_SIZE);
+    const outsize = PLAIN_GROUP_SIZE * ngroups;
+
+    const out = try gpa.alloc(u8, outsize);
+
+    _ = out;
+
+    return &[0]u8{};
+}
+
+test "encode" {
+    var allocator = std.heap.DebugAllocator(.{}).init;
+    const gpa = allocator.allocator();
+
+    const b64 = try encode(gpa, "henlo fren");
+    defer gpa.free(b64);
+
+    try std.testing.expectEqualSlices(u8, b64, "aGVubG8gZnJlbg==");
+}
