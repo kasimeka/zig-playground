ba92aa5fc3259f1f9899e38feee3784786ab7183
ورد <kasimeka@tuta.io>
2025-11-01 07:17:56 +0200

    init decoder

--- src/main.zig
+++ src/main.zig
@@ -10,12 +10,19 @@ pub fn main() !void {
     var allocator = std.heap.DebugAllocator(.{}).init;
     const gpa = allocator.allocator();
 
-    const str = try stdin.interface.allocRemaining(gpa, .unlimited);
-    const b64 = try base64.encode(gpa, str);
-    defer gpa.free(b64);
+    const argv = try std.process.argsAlloc(gpa);
+    var op = &base64.encode;
+    if (argv.len > 1 and std.mem.eql(u8, argv[1][0..2], "-d")) {
+        op = &base64.decode;
+    }
+    std.process.argsFree(gpa, argv);
+
+    const input = try stdin.interface.allocRemaining(gpa, .unlimited);
+    const output = try op(gpa, input);
+    defer gpa.free(output);
 
     var out_buf: [1.4 * buf_len]u8 = undefined;
     var stdout = std.fs.File.stdout().writer(&out_buf);
-    try stdout.interface.print("{s}\n", .{b64});
+    try stdout.interface.print("{s}\n", .{output});
     try stdout.interface.flush();
 }
--- src/root.zig
+++ src/root.zig
@@ -1,9 +1,17 @@
 const std = @import("std");
 
-pub const char_table: *const [64]u8 =
+pub const CHARS: *const [64]u8 =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ++
     "abcdefghijklmnopqrstuvwxyz" ++
     "0123456789+/";
+pub const CHARS_REV: [256]u8 = result: {
+    const INVALID_SENTINEL = 0;
+    var indices: [256]u8 = @splat(INVALID_SENTINEL);
+    for (0.., CHARS) |index, char| {
+        indices[char] = index;
+    }
+    break :result indices;
+};
 
 const PLAIN_GROUP_SIZE = 3;
 const ENCODED_GROUP_SIZE = 4;
@@ -14,19 +22,20 @@ pub fn encode(gpa: std.mem.Allocator, input: []const u8) ![]u8 {
     const group_count = try std.math.divCeil(usize, input.len, PLAIN_GROUP_SIZE);
     var output = try gpa.alloc(u8, ENCODED_GROUP_SIZE * group_count);
 
+    var group_buf: [PLAIN_GROUP_SIZE]u8 = undefined;
     for (0..group_count) |g| {
         const group_offset_in_input = PLAIN_GROUP_SIZE * g;
 
         const group_len = @min(PLAIN_GROUP_SIZE, input.len - group_offset_in_input);
-        var group_buf: [PLAIN_GROUP_SIZE]u8 = @splat(0);
         @memcpy(group_buf[0..group_len], input[group_offset_in_input..][0..group_len]);
+        @memset(group_buf[group_len..], 0);
 
         const group_offset_in_output = ENCODED_GROUP_SIZE * g;
         const out_group = output[group_offset_in_output..][0..ENCODED_GROUP_SIZE];
-        out_group[0] = char_table[group_buf[0] >> 2];
-        out_group[1] = char_table[(0b0011 & group_buf[0]) << 4 | group_buf[1] >> 4];
-        out_group[2] = char_table[(0b1111 & group_buf[1]) << 2 | group_buf[2] >> 6];
-        out_group[3] = char_table[0b00111111 & group_buf[2]];
+        out_group[0] = CHARS[group_buf[0] >> 2];
+        out_group[1] = CHARS[(0b0011 & group_buf[0]) << 4 | group_buf[1] >> 4];
+        out_group[2] = CHARS[(0b1111 & group_buf[1]) << 2 | group_buf[2] >> 6];
+        out_group[3] = CHARS[0b00111111 & group_buf[2]];
     }
 
     const pad_len =
@@ -38,24 +47,93 @@ pub fn encode(gpa: std.mem.Allocator, input: []const u8) ![]u8 {
     return output;
 }
 
-pub fn decode(gpa: std.mem.Allocator, str: []const u8) ![]u8 {
-    if (str.len == 0) return "";
-    const ngroups = try std.math.divCeil(usize, str.len, ENCODED_GROUP_SIZE);
-    const outsize = PLAIN_GROUP_SIZE * ngroups;
+pub fn decode(gpa: std.mem.Allocator, input: []const u8) ![]u8 {
+    if (input.len == 0) return "";
 
-    const out = try gpa.alloc(u8, outsize);
+    const group_count = try std.math.divCeil(usize, input.len, ENCODED_GROUP_SIZE);
+    const pad_len = result: for (0..PLAIN_GROUP_SIZE) |n| {
+        if (input[input.len - 1 - n] != '=') {
+            break :result n;
+        }
+    } else unreachable;
+    const output = try gpa.alloc(u8, PLAIN_GROUP_SIZE * group_count - pad_len);
 
-    _ = out;
+    var group_buf: [PLAIN_GROUP_SIZE]u8 = undefined;
+    for (0..group_count) |g| {
+        const group_offset_in_input = ENCODED_GROUP_SIZE * g;
+        const in_group = input[group_offset_in_input..];
+        const group_offset_in_output = PLAIN_GROUP_SIZE * g;
 
-    return &[0]u8{};
+        group_buf[0] =
+            (CHARS_REV[in_group[0]] << 2) | (0b0011 & (CHARS_REV[in_group[1]] >> 4));
+        group_buf[1] =
+            (CHARS_REV[in_group[1]] << 4) | (0b1111 & (CHARS_REV[in_group[2]] >> 2));
+        group_buf[2] =
+            (CHARS_REV[in_group[2]] << 6) | (0b00111111 & (CHARS_REV[in_group[3]]));
+
+        const out_group = output[group_offset_in_output..];
+        const group_len = @min(PLAIN_GROUP_SIZE, out_group.len);
+        @memcpy(out_group[0..group_len], group_buf[0..group_len]);
+    }
+
+    return output;
 }
 
 test "encode" {
-    var allocator = std.heap.DebugAllocator(.{}).init;
-    const gpa = allocator.allocator();
+    var gpa = std.heap.DebugAllocator(.{}).init;
+    var allocator = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer allocator.deinit();
+    const arena = allocator.allocator();
 
-    const b64 = try encode(gpa, "henlo fren");
-    defer gpa.free(b64);
+    try std.testing.expectEqualSlices(
+        u8,
+        "aGVubG8gZnJlbiwgaG93IHUgZG9pbiBmciBmcg==",
+        try encode(arena, "henlo fren, how u doin fr fr"),
+    );
+    try std.testing.expectEqualSlices(
+        u8,
+        "aGVubG8=",
+        try encode(arena, "henlo"),
+    );
+    try std.testing.expectEqualSlices(
+        u8,
+        "aGVubG9v",
+        try encode(arena, "henloo"),
+    );
+    try std.testing.expectEqualSlices(
+        u8,
+        "aGVubG9vbw==",
+        try encode(arena, "henlooo"),
+    );
+}
 
-    try std.testing.expectEqualSlices(u8, b64, "aGVubG8gZnJlbg==");
+test "decode" {
+    var gpa = std.heap.DebugAllocator(.{}).init;
+    var allocator = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer allocator.deinit();
+    const arena = allocator.allocator();
+
+    try std.testing.expectEqualSlices(
+        u8,
+        "Whereas recognition of the inherent dignity and of the equal and inalienable rights of all members of the human family is the foundation of freedom, justice and peace in the world",
+        try decode(
+            arena,
+            "V2hlcmVhcyByZWNvZ25pdGlvbiBvZiB0aGUgaW5oZXJlbnQgZGlnbml0eSBhbmQgb2YgdGhlIGVxdWFsIGFuZCBpbmFsaWVuYWJsZSByaWdodHMgb2YgYWxsIG1lbWJlcnMgb2YgdGhlIGh1bWFuIGZhbWlseSBpcyB0aGUgZm91bmRhdGlvbiBvZiBmcmVlZG9tLCBqdXN0aWNlIGFuZCBwZWFjZSBpbiB0aGUgd29ybGQ=",
+        ),
+    );
+    try std.testing.expectEqualSlices(
+        u8,
+        "henlo",
+        try decode(arena, "aGVubG8="),
+    );
+    try std.testing.expectEqualSlices(
+        u8,
+        "henloo",
+        try decode(arena, "aGVubG9v"),
+    );
+    try std.testing.expectEqualSlices(
+        u8,
+        "henlooo",
+        try decode(arena, "aGVubG9vbw=="),
+    );
 }
